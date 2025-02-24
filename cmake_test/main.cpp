e2669bb0dce93d747b5859c0197ecdfb20cd030d
elik565 <eliseysavich@gmail.com>
2025-02-24 10:06:50 -0500

    Несколько файлов в проекте

---
+++ cmake_test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main() {
+    std::cout << "Hello, world!\n";
+    send_message();
+
+    return 0;
+}
---
+++ cmake_test/utils.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "utils.hpp"
+
+void send_message() {
+  std::cout << "Hello from utils.cpp!\n";
+}
